a77674567d186ed59b3f4ca06bef7546008603d7
Alex68321 <38076020+Alex68321@users.noreply.github.com>
2018-04-10 21:20:31 +0200

    Update fin.py

--- fin.py
+++ fin.py
@@ -1,42 +1,39 @@
-import pygame as pg
 import pygame
-import random
 from settings import *
 
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Shmup!")
+pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 running = True
 
-vec = pg.math.Vector2
+vec = pygame.math.Vector2
 
-class Player(pg.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30, 40))
+class Player(pygame.sprite.Sprite):
+    def __init__(self,posx,posy):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(posx, posy)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
 
     def jump(self):
         # jump only if standing on a platform
         self.rect.x += 1
-        hits = pg.sprite.spritecollide(self, platforms, False)
+        hits = pygame.sprite.spritecollide(self, platforms, False)
         self.rect.x -= 1
         if hits:
             self.vel.y = -20
 
     def update(self):
         self.acc = vec(0, PLAYER_GRAV)
-        keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT]:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
             self.acc.x = -PLAYER_ACC
-        if keys[pg.K_RIGHT]:
+        if keys[pygame.K_RIGHT]:
             self.acc.x = PLAYER_ACC
 
         # apply friction
@@ -46,16 +43,42 @@ class Player(pg.sprite.Sprite):
         self.pos += self.vel + 0.5 * self.acc
         # wrap around the sides of the screen
         if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
             self.pos.x = WIDTH
+        if self.pos.x < 0:
+            self.pos.x = 0
 
         self.rect.midbottom = self.pos
 
-class Platform(pg.sprite.Sprite):
+    def shoot(self):
+    	bullet = Bullet(self.rect.centerx, self.rect.centery)
+    	all_sprites.add(bullet)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        
+        if player.vel.x >= 0:
+        	self.speedx = 10
+        elif player.vel.x < 0:
+        	self.speedx = -10
+        
+
+    def update(self):
+        self.rect.x += self.speedx
+        
+        if self.rect.bottom < 0:
+            self.kill()
+
+class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
-        pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((w, h))
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((w, h))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -63,8 +86,9 @@ class Platform(pg.sprite.Sprite):
 
 all_sprites = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+player = Player(WIDTH/4,HEIGHT/2)
+player2 = Player((WIDTH/4)*3,HEIGHT/2)
+all_sprites.add(player,player2)
 for plat in PLATFORM_LIST:
     p = Platform(*plat)
     all_sprites.add(p)
@@ -79,16 +103,38 @@ def events():
 			running = False
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE:
+				player.shoot()
+			if event.key == pygame.K_UP:
 				player.jump()
 
-def update():
-	all_sprites.update()
+def test(player):
+	pass
 	if player.vel.y > 0:
 		hits = pygame.sprite.spritecollide(player, platforms, False)
 		if hits:
 			player.pos.y = hits[0].rect.top
 			player.vel.y = 0
 
+	if player.rect.top <= HEIGHT / 4:
+		player.pos.y += abs(player.vel.y)
+		for plat in platforms:
+			plat.rect.y += abs(player.vel.y)
+			if plat.rect.top >= HEIGHT:
+				plat.kill()
+
+	if player.rect.bottom > HEIGHT:
+		for sprite in all_sprites:
+			sprite.rect.y -= max(player.vel.y, 10)
+			if sprite.rect.bottom < 0:
+				sprite.kill()
+	if len(platforms) == 0:
+		playing = False
+
+def update():
+	all_sprites.update()
+	test(player)
+	test(player2)
+	
 def draw():
 	screen.fill(BLACK)
 	all_sprites.draw(screen)
